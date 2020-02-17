15bd73363cf4613c5d6a8e89aefa92f77b5113ba
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-02-16 23:21:24 -0500

    24-Hour Change

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -26,9 +26,9 @@ public class ClockDisplay
     {
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
-        updateDisplay();
+        get24HourInternalDisplay();
     }
-
+    
     /**
      * Constructor for ClockDisplay objects. This constructor
      * creates a new clock set at the time specified by the 
@@ -40,7 +40,7 @@ public class ClockDisplay
         minutes = new NumberDisplay(60);
         setTime(hour, minute);
     }
-
+    
     /**
      * This method should get called once every minute - it makes
      * the clock display go one minute forward.
@@ -51,7 +51,7 @@ public class ClockDisplay
         if(minutes.getValue() == 0) {  // it just rolled over!
             hours.increment();
         }
-        updateDisplay();
+        get24HourInternalDisplay();
     }
 
     /**
@@ -62,7 +62,7 @@ public class ClockDisplay
     {
         hours.setValue(hour);
         minutes.setValue(minute);
-        updateDisplay();
+        get24HourInternalDisplay();
     }
 
     /**
@@ -76,9 +76,18 @@ public class ClockDisplay
     /**
      * Update the internal string that represents the display.
      */
-    private void updateDisplay()
+    private void get24HourInternalDisplay()
     {
-        displayString = hours.getDisplayValue() + ":" + 
-                        minutes.getDisplayValue();
+        int hour = hours.getValue();
+        String ending = "AM";
+        if (hour >= 12) { 
+            hour = hour - 12;
+            ending = "PM";
+        }
+        if (hour == 0) {
+            hour = 12;
+        }
+        displayString = hour + ":" + 
+                        minutes.getDisplayValue() + " " + ending;
     }
 }
